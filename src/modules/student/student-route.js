52c23c36d64ed721c6dd38fafe64efc2046a9b70
ngvozdetskyi <nikita.gvozdetskyi@gmail.com>
2023-05-31 23:16:06 +0300

    added auth

--- src/modules/student/student-route.js
+++ src/modules/student/student-route.js
@@ -3,7 +3,7 @@ const studentController = require('./student-controller');
 const { create, remove, update } = studentController;
 
 module.exports = async function (fastify) {
-    fastify.post('/student/create', { schema: createStudent }, create.bind(studentController));
-    fastify.post('/student/remove', { schema: removeStudent }, remove.bind(studentController));
+    fastify.post('/student/create', { schema: createStudent, preHandler: [fastify.verifyToken, fastify.guard.role(['admin'])] }, create.bind(studentController));
+    fastify.post('/student/remove', { schema: removeStudent, preHandler: [fastify.verifyToken, fastify.guard.role(['admin'])] }, remove.bind(studentController));
     fastify.post('/student/update', { schema: updateStudent }, update.bind(studentController));
 };
